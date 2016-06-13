100a79cb770c2afa28afd85173750700b9491826
leoschet <leoschettini2@gmail.com>
2016-06-13 10:32:50 -0300

    Implementation of array direct access and concat array functions and Refactoring of static array functions

--- Main.hs
+++ Main.hs
@@ -37,16 +37,57 @@ evalExpr env (ArrayLit (x:xs)) = do
     (Array b) <- evalExpr env (ArrayLit xs)
     return $ Array ([a] ++ b)
 
-evalExpr env (CallExpr (VarRef (Id name)) (x:xs) ) = do
-    p <- evalExpr env x
+-- global 'static' functions
+evalExpr env (CallExpr name params) = do
+    -- if name isn't a VarRef, it is not a function
     case name of
-        "head" -> case p of 
-            (Array [])-> return $ Array []
-            (Array (x:xs)) -> return x 
-        "tail" -> case p of
-            (Array [])-> return $ Array []
-            (Array (x:xs)) -> return $ Array xs
+        VarRef (Id fName) -> do
+            -- get first param
+            Array vals <- evalExpr env (head params)
+            case fName of
+                "head" ->
+                    -- if null, then the function has only one parameter
+                    -- if not null, it has more 
+                    if (null (tail params)) then
+                        return (head vals)
+                    else
+                        error $ "Too many arguments"
+                "tail" -> 
+                    if (null (tail params)) then
+                        return (Array (tail vals))
+                    else
+                        error $ "Too many arguments"
+                "len" -> 
+                    if (null (tail params)) then
+                        arraySize (Array vals) (Int 0)
+                    else
+                        error $ "Too many arguments"
+                "concat" -> do
+                    -- if null, it has only one parameter
+                    if (null (tail params)) then
+                        error $ "Too few arguments"
+                    else do
+                        Array valsToConcat <- evalExpr env (head (tail params))
+                        -- if null, the function has two parameters
+                        -- if not null, it has more
+                        if (null (tail (tail params))) then
+                            return (Array (vals ++ valsToConcat))
+                        else
+                            error $ "Too many arguments"
+                _ -> error $ show fName ++ " is not a function"
 
+
+-- Access array item using brackets
+evalExpr env (BracketRef expr idExpr) = do
+    obj <- evalExpr env expr
+    case obj of
+        Array array -> do
+            id <- evalExpr env idExpr
+            case id of
+                Int index -> getArrayByIndex env (Array array) (Int index)
+                _ -> error $ "Illegal argument type"
+        -- TODO: implement access to object properties
+        _ -> error $ "Illegal type"
 -----------------------------------------------------------------------------------
 
 evalStmt :: StateT -> Statement -> StateTransformer Value
@@ -191,6 +232,24 @@ assignOp env OpAssignBAnd v1 v2 = infixOp env OpBAnd v1 v2
 assignOp env OpAssignBXor v1 v2 = infixOp env OpBXor v1 v2
 assignOp env OpAssignBOr v1 v2 = infixOp env OpBOr v1 v2
 
+--
+-- Array auxiliary functions
+--
+
+getArrayByIndex :: StateT -> Value -> Value -> StateTransformer Value
+getArrayByIndex env (Array []) (Int _) = error $ "Array index out of bounds"
+getArrayByIndex env (Array (x:xs)) (Int index) =
+    if index < 0 then 
+        error $ "Negative index"
+    else 
+        if index == 0 then
+            return x
+        else
+            getArrayByIndex env (Array xs) (Int (index-1))
+
+arraySize :: Value -> Value -> StateTransformer Value
+arraySize (Array []) (Int len) = return $ Int $ len
+arraySize (Array (x:xs)) (Int len) = arraySize (Array xs) (Int (len+1))
 --
 -- Environment and auxiliary functions
 --
