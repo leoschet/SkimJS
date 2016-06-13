31db6bb05bbe20ae30e2399d3032340a3768b25e
Thayonara <tpa@cin.ufpe.br>
2016-06-13 17:05:44 -0300

    Compare the arrays

--- Main.js
+++ Main.js
@@ -1,3 +1,11 @@
 var l = [1,2,3];
+var l1 = [1,3,4];
+var l2 = [];
+var l5 = [1,2];
+var l3 = [1,2,3];
 var hl = head(l);
 var hl = tail(l);
+var t = (l == l1);
+var u = (l == l2);
+var w = (l == l3);
+var k = (l == l5);
