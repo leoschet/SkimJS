6a992bdbd95653c14984f468022f67f66c8b33b9
JoaoSant0s <ricardoelric@gmail.com>
2016-06-14 00:00:15 -0300

    Implementing the process functions. But unbounded scope.

--- Main.js
+++ Main.js
@@ -1,3 +1,18 @@
-var l = [1,2,3];
-var hl = head(l);
-var hl = tail(l);
+var x = [11, 4, 2, 1, 2, 5];
+var y = ["a", "4", "6", "9"];
+var z = 10;
+head(x);
+/*
+tail(y);
+*/
+//len(x);
+
+
+function auxFunc(number1,  number2) {
+	var k = 14;
+	k = k + number1;	
+	z = z + number2;
+	return number1 + number2;
+}
+
+auxFunc(4, 1);
